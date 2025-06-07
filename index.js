f392d58b61c08006d7c1d192a83de945445887bc
Shamba Chakraborty <world-null>
2025-06-07 19:31:07 +0530

    add logger morgen and wiston

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import 'dotenv/config'
 import express from 'express'
-
+import logger from './logger.js'
+import morgan from 'morgan'
 
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
+
+const morganFormat = ':method :url :status :response-time ms'
+
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message) => {
+            const logObject = {
+                method: message.split(" ")[0],
+                url: message.split(" ")[1],
+                status: message.split(" ")[2],
+                responseTime: message.split(" ")[3]
+            }
+            logger.info(JSON.stringify(logObject    ))
+        }
+    }
+}))
+
 let teaData = []
 let nextId = 1
 
